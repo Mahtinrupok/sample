0eed206f4dcafd8cf7f5de0a21f0fcc0ed163f87
Mahtinrupok <82091449+Mahtinrupok@users.noreply.github.com>
2021-08-09 12:50:38 +0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,16 +1,44 @@
+import cv2
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn import tree
-df=pd.read_csv('data_5.csv')
-X = df.values[:, 1:4]
-Y = df.values[:,0]
-X_train, X_test, y_train, y_test= train_test_split( X, Y, test_size= 0.3, random_state= 100)
-clf_entropy= DecisionTreeClassifier(criterion = "entropy", random_state= 100, 
-max_depth=2, min_samples_leaf=3)
-clf_entropy.fit(X_train, y_train)
-y_pred_en= clf_entropy.predict(X_test)
-print(y_pred_en)
-print("Accuracy is ", accuracy_score(y_test,y_pred_en)*100)
+MIN_MATCH_COUNT=30
+
+detector=cv2.SIFT()
+
+FLANN_INDEX_KDITREE=0
+flannParam=dict(algorithm=FLANN_INDEX_KDITREE,tree=5)
+flann=cv2.FlannBasedMatcher(flannParam,{})
+
+trainImg=cv2.imread("TrainingData/TrainImg.jpeg",0)
+trainKP,trainDesc=detector.detectAndCompute(trainImg,None)
+
+cam=cv2.VideoCapture(0)
+while True:
+    ret, QueryImgBGR=cam.read()
+    QueryImg=cv2.cvtColor(QueryImgBGR,cv2.COLOR_BGR2GRAY)
+    queryKP,queryDesc=detector.detectAndCompute(QueryImg,None)
+    matches=flann.knnMatch(queryDesc,trainDesc,k=2)
+
+    goodMatch=[]
+    for m,n in matches:
+        if(m.distance<0.75*n.distance):
+            goodMatch.append(m)
+    if(len(goodMatch)>MIN_MATCH_COUNT):
+        tp=[]
+        qp=[]
+        for m in goodMatch:
+            tp.append(trainKP[m.trainIdx].pt)
+            qp.append(queryKP[m.queryIdx].pt)
+        tp,qp=np.float32((tp,qp))
+        H,status=cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
+        h,w=trainImg.shape
+        trainBorder=np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
+        queryBorder=cv2.perspectiveTransform(trainBorder,H)
+        cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
+    else:
+        print "Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT)
+    cv2.imshow('result',QueryImgBGR)
+    if cv2.waitKey(10)==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
+ 
